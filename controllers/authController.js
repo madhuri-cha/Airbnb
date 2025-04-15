e4a9103f6ca0933b5689c8982ca02220799b2030
madhuri-cha <madhurichavan15122003@gmail.com>
2025-04-15 23:07:42 +0530

    sign up and sign in pages for authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,3 +23,97 @@ exports.postLogout=(req, res, next)=>{
     }); // Destroy the session
    
 }
+exports.getSignup=(req,res,next)=>{
+    res.render('auth/signup', {
+        pageTitle:'signup',
+        currentPage:'signup',
+        isLoggedIn:false,
+        errors:[],
+        oldInput:{
+            username:'',
+            email:'',
+            password:'',
+            userType:'',
+            confirmPassword:''
+        }
+    })
+}
+
+exports.postSignup=[
+    
+    check("username")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 5})
+    .withMessage("Username should be at least 5 characters long")
+    .mathces(/^[a-zA-Z\s]+$/)
+    .withMessage("Username should be contain only letters")
+    ,
+   
+    check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email address")
+    .normalizeEmail()
+    ,
+
+    check("password")
+    .isLength({min:8})
+    .withMessage("Password should be 8 characters Long")
+    .matches(/[A-Z]/)
+    .withMessage("Password should contain at least one uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password should contain at least one lowercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password should contain at least one number")
+    .matches(/[!@#$%^&*]/)
+    .withMessage("Password should contain at least one special character")
+    .trim()
+    ,
+
+    check("confirmPassword")
+    .trim()
+    .custom((value, {req})=>{
+        if(value !== req.body.password)
+        {
+            throw new Error("password and confirm password should be same")
+        }
+        return true;
+    })
+    ,
+    check('userType')
+    .notEmpty()
+    .withMessage("Please select a userType")
+    .isIn(['host', 'guest'])
+    .withMessage("Please select a valid userType")
+     ,
+     check('terms')
+     .notEmpty()
+     .withMessage("Please accept the terms and conditions")
+     .custom((value, {req})=>{
+        if(value !== 'on')
+        {
+            throw new Error("Please accept the terms and conditions")
+        }
+        return true;
+     }),
+
+    (req, res, next)=>{
+        const errors=validationResult(req);
+        if(!errors.isEmpty())
+        {
+            return res.status(422).renser('auth/signup',
+                {
+                    pageTitle: 'signup',
+                    currentPage: 'signup',
+                    isLoggedIn: false,
+                    errorMessages: errors.array().map((err)=> err.msg),
+                    oldInput:
+                    {
+                      username, email, password, userType
+                    }
+                });
+        }
+        res.redirect('/login')
+    }
+
+    ]
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -6,5 +6,7 @@ const authController = require('../controllers/authController.js');
 authRouter.get('/login', authController.getLogin);
 authRouter.post('/login', authController.postLogin); // ✅ Add POST handler
 authRouter.get('/logout', authController.postLogout); // ✅ Add logout handler
+authRouter.get('/signup', authController.getSignup); // ✅ Add signup handler
+authRouter.post('/signup', authController.postSignup); // ✅ Add signup handler
 
 module.exports = authRouter; // ✅ Export the router directly
